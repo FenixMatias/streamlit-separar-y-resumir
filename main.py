233209cfa235a37e680332e94cbdd56bf739b942
FenixMatias <matiaszodiaco@gmail.com>
2024-08-01 02:09:45 -0400

    Resolved merge conflicts

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import streamlit as st
-from io import StringIO
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import load_summarize_chain
+from langchain import PromptTemplate
 from langchain_openai import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pandas as pd
+from io import StringIO
 
-# Asegúrate de tener tu clave API de OpenAI
-openai_api_key = st.text_input("Introduce tu clave API de OpenAI", type="password")
+#LLM y función de carga de llaves
+def load_LLM(openai_api_key):
+    # Asegúrese de que su openai_api_key se establece como una variable de entorno
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    return llm
 
-uploaded_file = st.file_uploader("Sube tu archivo")
+
+#Título y cabecera de la página
+st.set_page_config(page_title="Resumidor de textos largos AI")
+st.header("Resumidor de Textos Largos AI")
+
+
+#Intro: instrucciones
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("ChatGPT no puede resumir textos largos. Ahora puedes hacerlo con esta aplicación.")
+
+with col2:
+    st.write("Contacte con [Matias Toro Labra](https://www.linkedin.com/in/luis-matias-toro-labra-b4074121b/) para construir sus proyectos de IA")
+
+
+#Introducir la clave API de OpenAI
+st.markdown("## Introduzca su clave API de OpenAI")
+
+def get_openai_api_key():
+    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input", type="password")
+    return input_text
+
+openai_api_key = get_openai_api_key()
+
+
+# Entrada
+st.markdown("## Cargue el archivo de texto que desea resumir")
+
+uploaded_file = st.file_uploader("Elija un archivo", type="txt")
+
+       
+# Salida
+st.markdown("### Este es su resumen:")
 
 if uploaded_file is not None:
     # Leer el archivo como bytes
